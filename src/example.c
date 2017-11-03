63d890b537d67200afd6ee4d320de0e76276d1c7
autocommit <autocommit>
2017-11-03 01:29:54 +0000

    auto 0 33 319

---
+++ src/example.c
@@ -0,0 +1,18 @@
+int main(int argc, char *argv[])
+{
+#include "o/foo.gen.h"
+	enum foo foo = lookup_foo(argv[1]);
+	switch(foo) {
+	case foo_BAR:
+		puts("foobar");
+		break;
+	case foo_BAZ:
+		puts("bazz");
+		break;
+	case foo_UNKNOWN:
+		printf("ehunno! what is %s?",argv[1]);
+	default:
+		printf("We should really handle %d(%s)\n",foo,argv[1]);
+	};
+	return 0;
+}
